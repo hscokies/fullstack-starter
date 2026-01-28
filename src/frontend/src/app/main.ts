26782697b3693cbe897a67c6516a7cbfb0add1de
hscokies <hscokies@outlook.com>
2026-01-28 23:20:01 +0500

    feat: add feather icons

--- src/frontend/src/app/main.ts
+++ src/frontend/src/app/main.ts
@@ -3,6 +3,12 @@ import { classNamePlugin } from '@/shared/plugins';
 import { createApp } from 'vue';
 import App from './ui/App.vue';
 import { i18n } from '@/shared/i18n';
+import VueFeather from 'vue-feather';
 import '@/app/ui/global.css';
 
-createApp(App).use(i18n).use(router).use(classNamePlugin).mount('#app');
+const app = createApp(App);
+app.component('feather', VueFeather);
+
+app.use(i18n).use(router).use(classNamePlugin);
+
+app.mount('#app');
